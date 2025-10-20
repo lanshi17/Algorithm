0af83f83dbbfe634ce135fbab8d8f105b545a606
lanshi17 <yhvguk@foxmail.com>
2025-10-21 00:00:57 +0800

    backup

--- .work_logs.sh
+++ .work_logs.sh
@@ -5,3 +5,6 @@ git commit -m "first commit"\
 git branch -M master\
 git remote add origin git@github.com:lanshi17/Algorithm.git\
 git push -u origin master
+: 1760321615:0;git push -rebase
+: 1760321624:0;git push -v
+: 1760976042:0;git add .
